bc68dd78d2b9d6940180f87bc06cc77591070cb2
Jhonny Asto <133028636+JhonnyASTOatoccsa@users.noreply.github.com>
2023-06-09 10:02:54 -0500

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,16 @@
+
+edad = 0 
+while edad != 20:
+    edad=int(input("ingrese_edad"))
+
+
+
+
+
+
+
+
+nombre = "jose"
+while nombre == "jose":
+    nombre = input("ingrese_nombre: ")
+    print("hola",nombre + "!")
